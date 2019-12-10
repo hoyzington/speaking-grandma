93862d1a8b4258d05cec1d4eb01682ef0eea719b
Scott Hoisington <hoyzington@gmail.com>
2019-12-10 09:01:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,9 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
-    return ""
+    return "NO, NOT SINCE 1938!"
+  else
+    return 
 end
 
 # Whatever you say to grandma, she should respond with
